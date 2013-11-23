422f3fd7a63290ace90309a153a8a98dcb6ca98a
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2013-11-22 23:07:18 -0600

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="attrdict",
-    version="0.1.0",
+    version="0.1.1",
     author="Brendan Curran-Johnson",
     author_email="brendan@bcjbcj.ca",
     packages=["attrdict", "attrdict.test"],

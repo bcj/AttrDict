25d9ee01e8e2a97888c9cbdc379dffa534246002
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-03-19 11:12:07 -0500

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="attrdict",
-    version="0.2.0",
+    version="0.2.1",
     author="Brendan Curran-Johnson",
     author_email="brendan@bcjbcj.ca",
     packages=["attrdict", "attrdict.test"],

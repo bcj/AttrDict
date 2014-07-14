5e8814c08e8604fcef5f96681cb264fc23738960
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-07-14 14:10:53 -0500

    0.5.1 (hot on the trail of 0.5.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="attrdict",
-    version="0.5.0",
+    version="0.5.1",
     author="Brendan Curran-Johnson",
     author_email="brendan@bcjbcj.ca",
     packages=["attrdict"],

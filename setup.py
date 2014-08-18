2ccfbce395a1c0a71feb286a48824e0fda850c98
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-08-18 09:22:21 -0500

    VERSION 1.0.0
    I feel comfortable calling this package production ready. No code changes for this release, but trove classifiers have finally been added, as has support for wheels (https://www.youtube.com/watch?v=r9biPKTozTg).

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="attrdict",
-    version="0.5.1",
+    version="1.0.0",
     author="Brendan Curran-Johnson",
     author_email="brendan@bcjbcj.ca",
     packages=["attrdict"],
@@ -10,4 +13,17 @@ setup(
     license="MIT License",
     description="A dict with attribute-style access",
     long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
 )
